296f7d6fbd9c5fcac89a4617a6dfaef871f9f7a8
Jônatas Lago <89043103+jonataslago@users.noreply.github.com>
2023-05-15 21:17:10 -0300

    Create Monster Card V2

---
+++ monster-card/functions.js
@@ -0,0 +1,63 @@
+function editar(){
+
+    nome.innerText = newNome.value;
+
+    grau.innerText = newGrau.value;
+
+    nvlMonstro.innerText = newNvlMonstro.value;
+
+    observation.innerText = newObservation.value;
+
+    desafio.innerText = newDesafio.value;
+
+    forca.innerText = newForca.value;
+
+    destreza.innerText = newDestreza.value;
+
+    constituicao.innerText = newConstituicao.value;
+
+    inteligencia.innerText = newInteligencia.value;
+
+    sabedoria.innerText = newSabedoria.value;
+
+    carisma.innerText = newCarisma.value;
+
+    especialidade.innerText = newEspecialidade.value;
+
+}
+
+
+
+
+function criar(){
+
+    const file = img.files[0];
+    const reader = new FileReader();
+
+    reader.readAsDataURL(file);
+
+    reader.onload = () => {
+        imagem.src = reader.result;
+    }
+
+    editar();
+}
+
+function exportar() {
+
+    const canva = carlos.querySelector('canvas');
+  
+    if (canva){
+  
+      canva.remove();
+  
+    }else{
+  
+      html2canvas(document.querySelector("#capture")).then(canvas => {
+        carlos.appendChild(canvas)
+      });
+  
+    }
+  
+    
+  }
---
+++ monster-card/var.js
@@ -0,0 +1,43 @@
+let carlos = document.querySelector('#carlos');
+
+const input = document.querySelector('#valor');
+const barra = document.querySelector('#barra');
+
+const img = document.querySelector('#imagemInput');
+const imagem = document.querySelector('#imagemExibida');
+
+let nome = document.querySelector('#nome');
+let newNome = document.querySelector('#newNome');
+
+let grau = document.querySelector('#grau');
+let newGrau = document.querySelector('#newGrau');
+
+let nvlMonstro = document.querySelector('#nvlMonstro');
+let newNvlMonstro = document.querySelector('#newNvlMonstro');
+
+let observation = document.querySelector('#observation');
+let newObservation = document.querySelector('#newObservation');
+
+let desafio = document.querySelector('#desafio');
+let newDesafio = document.querySelector('#newDesafio');
+
+let forca = document.querySelector('#forca');
+let newForca = document.querySelector('#newForca');
+
+let destreza = document.querySelector('#destreza');
+let newDestreza = document.querySelector('#newDestreza');
+
+let constituicao = document.querySelector('#constituicao');
+let newConstituicao = document.querySelector('#newConstituicao');
+
+let inteligencia = document.querySelector('#inteligencia');
+let newInteligencia = document.querySelector('#newInteligencia');
+
+let sabedoria = document.querySelector('#sabedoria');
+let newSabedoria = document.querySelector('#newSabedoria');
+
+let carisma = document.querySelector('#carisma');
+let newCarisma = document.querySelector('#newCarisma');
+
+let especialidade = document.querySelector('#especialidade');
+let newEspecialidade = document.querySelector('#newEspecialidade');
